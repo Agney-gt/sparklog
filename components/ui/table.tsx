40029aa7a9c414e1f0bf14cccd8ba53a1c4582bf
Agney <agneynalapat123@gmail.com>
2024-11-20 21:41:02 +0530

    sonarcloud reliability fixes

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -4,12 +4,19 @@ import { cn } from "@/lib/utils"
 
 const Table = React.forwardRef<
   HTMLTableElement,
-  React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
+  React.HTMLAttributes<HTMLTableElement> & {
+    /**
+     * Set to true if the table is used for layout purposes only.
+     * This will add role="presentation" to make it clear to assistive technologies.
+     */
+    isPresentation?: boolean
+  }
+>(({ className, isPresentation, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
+      role={isPresentation ? "presentation" : "table"}
       {...props}
     />
   </div>
@@ -20,9 +27,31 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead 
+    ref={ref} 
+    className={cn("[&_tr]:border-b", className)}
+    role="rowgroup"
+    {...props} 
+  />
 ))
 TableHeader.displayName = "TableHeader"
+
+const TableHead = React.forwardRef<
+  HTMLTableCellElement,
+  React.ThHTMLAttributes<HTMLTableCellElement>
+>(({ className, ...props }, ref) => (
+  <th
+    ref={ref}
+    className={cn(
+      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      className
+    )}
+    scope="col"
+    role="columnheader"
+    {...props}
+  />
+))
+TableHead.displayName = "TableHead"
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
@@ -66,20 +95,6 @@ const TableRow = React.forwardRef<
 ))
 TableRow.displayName = "TableRow"
 
-const TableHead = React.forwardRef<
-  HTMLTableCellElement,
-  React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
-  <th
-    ref={ref}
-    className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
-      className
-    )}
-    {...props}
-  />
-))
-TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
