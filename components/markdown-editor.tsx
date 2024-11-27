5ce27bbdbdfc3afa4862c86f155e173b5ee94b7f
Agney <agneynalapat123@gmail.com>
2024-11-27 19:06:11 +0530

    v3 sparklog

--- components/markdown-editor.tsx
+++ components/markdown-editor.tsx
@@ -17,7 +17,7 @@ export default function MarkdownEditor({ value, onChange }: MarkdownEditorProps)
         enableScroll
         height={400}
         textareaProps={{
-          placeholder: "Reflect and create an amazing markdown journal entry with the Chatbot! 🚀 Try a Quest by simply copying and pasting it into the Chatbot. Begin your journal entry with the quest title as a header, such as # A Noble Crown or # Shining Brightly. Quests will be marked complete after three days of journaling.",
+          placeholder: "Coding, Markdown, and Journaling have one thing in common—they’re like assembling a puzzle. Each piece builds on the last to reveal a bigger picture. \n\nCopy-paste Quests into the chatbot, create a template you like and title each journal entry with the name of the quest -  (e.g., # A Noble Crown or # Shining Brightly).",
           'aria-label': 'Markdown editor',
         }}
 
