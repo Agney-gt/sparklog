c73373e5cbf53e42f3ac9e581ac0c520528e19e8
Agney <agneynalapat123@gmail.com>
2024-11-27 12:01:48 +0530

    mdfixes

--- components/markdown-editor.tsx
+++ components/markdown-editor.tsx
@@ -14,6 +14,13 @@ export default function MarkdownEditor({ value, onChange }: MarkdownEditorProps)
       <MDEditor
         value={value}
         onChange={(val) => onChange(val || '')}
+        enableScroll
+        height={400}
+        textareaProps={{
+          placeholder: "Reflect and create an amazing markdown journal entry with the Chatbot! 🚀 Try a Quest by simply copying and pasting it into the Chatbot. Begin your journal entry with the quest title as a header, such as # A Noble Crown or # Shining Brightly. Quests will be marked complete after three days of journaling.",
+          'aria-label': 'Markdown editor',
+        }}
+
       />
     </div>
   );
