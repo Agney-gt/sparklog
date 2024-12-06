2562a82aa765f2484586a77060beb74e97d1d629
Agney <agneynalapat123@gmail.com>
2024-12-06 20:56:47 +0530

    unused vars

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -12,7 +12,6 @@ import { ActionWorkbook } from "@/components/action-workbook"
 import Banner from "@/components/banner"
 import LevelProgress from '@/components/level-progress'
 import QuestLog from "@/components/quest-log"
-import TechStartupBattle from "@/components/tech-startup-battle"
 export default function Component() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
   const [markedDates, setMarkedDates] = React.useState<string[]>([])
