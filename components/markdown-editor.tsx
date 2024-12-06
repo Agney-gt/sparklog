837f9d8ca5511b5f3c4f582fac375056697608c9
Agney <agneynalapat123@gmail.com>
2024-12-06 14:07:14 +0530

    added copypasteimages

--- components/markdown-editor.tsx
+++ components/markdown-editor.tsx
@@ -1,19 +1,62 @@
 'use client'
-
 import React from "react";
 import MDEditor from '@uiw/react-md-editor';
 
 interface MarkdownEditorProps {
   value: string;
   onChange: (value: string) => void;
+  
 }
 
 export default function MarkdownEditor({ value, onChange }: MarkdownEditorProps) {
+  // Image upload to server
+const imageUploadHandler = async (
+  image: File
+): Promise<{ alt: string; url: string } | null> => {
+  if (image && image.size === 0) return null;
+  const formData = new FormData();
+  formData.append("image", image);
+  //formData.append("what else you want", "your value");
+  const response = await fetch('/api/user/images', {
+        method: 'POST',
+        body: formData
+      })
+  return  (await response.json()) as { alt: string, url: string }
+};
+
+const handlePaste = async (event: React.ClipboardEvent<HTMLDivElement>) => {
+  // Access the clipboard data using event.clipboardData
+  const clipboardData = event.clipboardData;
+ // only if clipboard payload is file
+  if (clipboardData.files.length === 1) {
+    const myfile = clipboardData.files[0] as File;
+   // you could perform some test: image size, type mime ....
+    const url = await imageUploadHandler(myfile);
+    event.preventDefault();
+    if (url) {
+     // change clipboard payload,
+     // document execCommand is obsolete, you could replace with navigator.clipboard API but some browser
+    // accept write() method only if the connexion is secure (https)...
+      document.execCommand(
+        "insertText",
+        false,
+        `![${url.alt}](${url.url})\n`
+      );
+    } else {
+      document.execCommand(
+        "insertText",
+        false,
+        "ERROR Image has not been stored on server"
+      );
+    }
+  }
+};
   return (
     <div className="container">
       <MDEditor
         value={value}
         onChange={(val) => onChange(val || '')}
+        onPaste={handlePaste}
         enableScroll
         height={400}
         textareaProps={{
