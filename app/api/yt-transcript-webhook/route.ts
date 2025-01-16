b08222db789db054d6af00f40258690b824c5766
Agney <agneynalapat123@gmail.com>
2025-01-16 10:00:47 +0530

    test

--- app/api/yt-transcript-webhook/route.ts
+++ app/api/yt-transcript-webhook/route.ts
@@ -1,7 +1,5 @@
 import { YoutubeTranscript } from 'youtube-transcript';
 import { NextResponse } from 'next/server'
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
 
 
 export async function POST(request: Request) {
