baa29b5c59bc82cee5a2bec61275167cbf1de25c
Agney <agneynalapat123@gmail.com>
2025-01-30 12:51:48 +0530

    updates

--- app/api/get-html/route.ts
+++ app/api/get-html/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
       ];
     
     try {
-        
+        console.log(request)
         const response = await fetch(htmlUrls[currentIndex], { cache: 'no-store' });
         if (!response.ok) {
           throw new Error(`Failed to fetch content: ${response.status}`);
--- components/wisdom-quiz.tsx
+++ components/wisdom-quiz.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
-import { quizDatas, type QuizQuestion } from "@/lib/quiz-data"
+import { type QuizQuestion } from "@/lib/quiz-data"
 
 export default function WisdomQuiz({ onComplete, userId, itemId }: { onComplete: () => void, userId: string, itemId: number }) {
   const [currentQuestion, setCurrentQuestion] = useState(0)
